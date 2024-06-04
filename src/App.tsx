41ce0aeda84c3eb30b6c5c4ab614266c337a3670
Quy3008 <quykinas1412@gmail.com>
2024-06-04 18:01:28 +0700

    sidebar menu

--- src/App.tsx
+++ src/App.tsx
@@ -8,22 +8,27 @@ import CoffeHolicTab from "./pages/ChuyenNhaPage/Tabs/CoffeeHome";
 import AllHomes from "./pages/ChuyenNhaPage/Tabs/AllHomes";
 import TeaHolicTab from "./pages/ChuyenNhaPage/Tabs/TeaHomes";
 import BlogTab from "./pages/ChuyenNhaPage/Tabs/BlogHomes";
+import AllSideBar from "./pages/MenuPage/SideBar/AllSide";
+import TeaSideBar from "./pages/MenuPage/SideBar/TeaSideBar";
 // import AllHomes from "./pages/ChuyenNhaPage/Tabs/AllHomes";
 // import "./pages/HomePage/HomePage.css"
 function App() {
   return (
-      <Routes>
-        <Route path="" element={<WebsiteLayout />}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="menu" element={<MenuPage />} />
-          <Route path="chuyennha" element={<ChuyenNhaPage />}>
-            <Route path="" element={<AllHomes />} />
-            <Route path="coffeeholic" element={<CoffeHolicTab />} />
-            <Route path="teaholic" element={<TeaHolicTab />} />
-            <Route path="blog" element={<BlogTab />} />
-          </Route>
+    <Routes>
+      <Route path="" element={<WebsiteLayout />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="menu" element={<MenuPage />}>
+          <Route path="" element={<AllSideBar />} />
+          <Route path="teaside" element={<TeaSideBar />} />
         </Route>
-      </Routes>
+        <Route path="chuyennha" element={<ChuyenNhaPage />}>
+          <Route path="" element={<AllHomes />} />
+          <Route path="coffeeholic" element={<CoffeHolicTab />} />
+          <Route path="teaholic" element={<TeaHolicTab />} />
+          <Route path="blog" element={<BlogTab />} />
+        </Route>
+      </Route>
+    </Routes>
   );
 }
 export default App
--- src/pages/MenuPage/MenuPage.tsx
+++ src/pages/MenuPage/MenuPage.tsx
@@ -1,9 +1,53 @@
-const MenuPage: React.FC = () => {
-    return (
-        <>
-            <img src="\src\assets\images\kane-house-dragon-ball_3840x2160_xtrafondos.com.jpg" alt="" />
-        </>
-    )
+import { useState } from "react";
+import { Link, Outlet } from "react-router-dom";
+
+
+interface SidebarItem {
+    text: string;
+    link?: string; // Optional link for the item
 }
 
-export default MenuPage
+const MenuPage: React.FC = () => {
+    const sidebarItems: SidebarItem[] = [
+        { text: "Tất cả", link: "" },
+        { text: "Trà", link: "teaside" },
+        { text: "Cà phê", link: "#" },
+        { text: "Hi-Tea Healthy", link: "#" },
+        { text: "Bánh & Snack", link: "#" },
+    ];
+
+    const [activeItem, setActiveItem] = useState<string | null>(null);
+
+    const handleClick = (text: string) => {
+        setActiveItem(text); // Update active item on click
+    };
+
+    return (
+        <>
+            <div>
+                <div className="containerAll mt-16 flex mx-auto">
+                    <div className="siderbar border-2 border-red-500 min-w-60">
+                        <ul className="text-left mx-6">
+                            {sidebarItems.map((item) => (
+                                <li key={item.text} className="py-2">
+                                    <Link
+                                        to={item.link || ""}
+                                        className={activeItem === item.text ? "active  text-orange-500 font-bold -translate-y-6" : ""}
+                                        onClick={() => handleClick(item.text)}
+                                    >
+                                        <h3 className=" ease-in-out hover:-translate-y-1" >{item.text}</h3>
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                    <div className="allproduct border-2 border-red-500 flex-1">
+                        <Outlet />
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default MenuPage;
